08d17150f3bfc1bd6e3cb5dddaf2837cde6e355f
bianca-loera <bianca.loera.062@my.csun.edu>
2024-07-23 00:07:03 -0700

    working on signup/login page

--- src/main/java/comp380project/Controller.java
+++ src/main/java/comp380project/Controller.java
@@ -1,10 +1,30 @@
 package comp380project;
 
 import javafx.event.ActionEvent;
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
-    public void login(ActionEvent e) {
-        System.out.println("username: password:");
+    public void open_signin(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("SignIn.fxml")); 
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void open_signup(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("SignUp.fxml")); 
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/comp380project/Main.java
+++ src/main/java/comp380project/Main.java
@@ -5,9 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.scene.text.Font;
 
 import java.io.IOException;
 
@@ -24,23 +21,14 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException { 
         //to create new stage: Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("main.fxml"));
-        Scene scene = new Scene(root,Color.WHITE); //screen color
+        Scene scene = new Scene(root); 
+        String css = this.getClass().getResource("/main.css").toExternalForm();
+        scene.getStylesheets().add(css);
+       
 
         stage.setTitle("Stock Price Prediction System"); //change name
-        //stage.setWidth(500); //changes size of screen
-        //stage.setHeight(500); //changes size of screen
         stage.setResizable(false);
-        //changes the location of the screen: stage.setX(); stage.setY();
         //makes the screen full stage: stage.setFullScreen(true);
-
-        Text text = new Text();
-        text.setText("HELLO");
-        text.setX(50); //changes the location of text
-        text.setY(50); //changes the location of text
-        text.setFont(Font.font("Times New Roman", 60)); //text font & size
-        text.setFill(Color.BLACK); //text color
-
-        
 
         //root.getChildren().add(text);
         stage.setScene(scene);
