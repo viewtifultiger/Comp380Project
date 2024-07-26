f8b2ad039570424bb82d693f8ec8c4834635ea2d
bianca-loera <bianca.loera.062@my.csun.edu>
2024-07-25 23:17:37 -0700

    more changes to login/signup

--- src/main/java/comp380project/Controller.java
+++ src/main/java/comp380project/Controller.java
@@ -2,11 +2,15 @@ package comp380project;
 
 import javafx.event.ActionEvent;
 import java.io.IOException;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 
 public class Controller {
     private Stage stage;
@@ -20,6 +24,7 @@ public class Controller {
         stage.setScene(scene);
         stage.show();
     }
+
     public void open_signup(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("SignUp.fxml")); 
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -27,4 +32,90 @@ public class Controller {
         stage.setScene(scene);
         stage.show();
     }
-}
+
+    // FXML's controls
+    @FXML
+    private Button invalidLogin;
+    @FXML
+    private Button invalidSignup;
+    //@FXML
+    //private Button cancelButton;
+    @FXML
+    private TextField loginTextField;
+    @FXML
+    private PasswordField loginPasswordField;
+    @FXML
+    private TextField signUpTextField;
+    @FXML
+    private TextField signUpEmailTextField;
+    @FXML
+    private PasswordField signUpPasswordField;
+    @FXML
+    private PasswordField signUpRepeatPasswordField;
+    
+    //@FXML
+    //protected void onCancelButtonClick() {
+       // Stage stage = (Stage) cancelButton.getScene().getWindow();
+       // stage.close();
+    //}
+    protected
+    String successMessage = String.format("-fx-text-fill: black;");
+    String errorMessage = String.format("-fx-text-fill: black;");
+    String errorStyle = String.format("-fx-border-color: black; -fx-border-width: 2; -fx-border-radius: 5;");
+    String successStyle = String.format("-fx-border-color: #A9A9A9; -fx-border-width: 2; -fx-border-radius: 5;");
+
+    @FXML
+    protected void onLoginButtonClick() {
+        if (loginTextField.getText().isBlank() || loginPasswordField.getText().isBlank()) {
+            invalidLogin.setText("The Login fields are required!");
+            invalidLogin.setStyle(errorMessage);
+            invalidSignup.setText("");
+
+            if (loginTextField.getText().isBlank()) {
+                loginTextField.setStyle(errorStyle);
+            } else if (loginPasswordField.getText().isBlank()) {
+                loginPasswordField.setStyle(errorStyle);
+            }
+        } else {
+            invalidLogin.setText("Login Successful!");
+            invalidLogin.setStyle(successMessage);
+            loginTextField.setStyle(successStyle);
+            loginPasswordField.setStyle(successStyle);
+            invalidSignup.setText("");
+        }
+    }
+
+    @FXML
+    protected void onSignUpButtonClick() {
+
+        if (signUpTextField.getText().isBlank() || signUpEmailTextField.getText().isBlank() || signUpPasswordField.getText().isBlank() || signUpRepeatPasswordField.getText().isBlank()) {
+            invalidSignup.setText("Please fill in all fields!");
+            invalidSignup.setStyle(errorMessage);
+            invalidLogin.setText("");
+
+            if (signUpTextField.getText().isBlank()) {
+                signUpTextField.setStyle(errorStyle);
+            } else if (signUpEmailTextField.getText().isBlank()) {
+                signUpEmailTextField.setStyle(errorStyle);
+            } else if (signUpPasswordField.getText().isBlank()) {
+                signUpPasswordField.setStyle(errorStyle);
+            } else if (signUpRepeatPasswordField.getText().isBlank()) {
+                signUpRepeatPasswordField.setStyle(errorStyle);
+            }
+        } else if (signUpRepeatPasswordField.getText().equals(signUpPasswordField.getText())) {
+            invalidSignup.setText("You are set!");
+            invalidSignup.setStyle(successMessage);
+            signUpTextField.setStyle(successStyle);
+            signUpEmailTextField.setStyle(successStyle);
+            signUpPasswordField.setStyle(successStyle);
+            signUpRepeatPasswordField.setStyle(successStyle);
+            invalidLogin.setText("");
+        } else {
+            invalidSignup.setText("Passwords don't match!");
+            invalidSignup.setStyle(errorMessage);
+            signUpPasswordField.setStyle(errorStyle);
+            signUpRepeatPasswordField.setStyle(errorStyle);
+            invalidLogin.setText("");
+        }
+    }
+}               
