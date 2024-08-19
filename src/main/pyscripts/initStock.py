62d9a6008c6d4af145095bb08fa37442f91697ab
= <=>
2024-08-18 17:37:33 -0700

    updates, cleaned up

--- src/main/pyscripts/initStock.py
+++ src/main/pyscripts/initStock.py
@@ -6,8 +6,12 @@ objDir = pt.dir.objDir
 csvDir = pt.dir.csvDir
 ticker = sys.argv[1]
 
-stock = yf.Ticker(f"^{ticker}") # initialize ticker class, S&P 500 ticker symbol
+stock = yf.Ticker(f"{ticker}") # initialize ticker class, S&P 500 ticker symbol
+
 stockTable = stock.history(period = "max")
+
+if (stockTable.empty == True):
+	raise Exception(f"{ticker} does not exist")
 
 pt.save_object(stock.info, f"{objDir}{ticker}_info")	# yfinance class
 pt.save_object(stockTable, f"{objDir}{ticker}_panda")	# pandas class
